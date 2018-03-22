b8d7d737bb6db46ba25d8eb529c5bc1f68b9e426
Ezequiel Smucler <ezequiels.90@gmail.com>
2018-03-21 18:14:16 -0700

    "wrap" odpc_priv so that it return a field; this will be made tidier

--- src/odpc_auxfunc.cpp
+++ src/odpc_auxfunc.cpp
@@ -383,66 +383,183 @@ List odpc_priv(const arma::mat & Z,
   return(ret);
 }
 
-// // [[Rcpp::export]]
-// List roll_odpc(const arma::field<arma::mat> & data_field,
-//                const arma::uword & k,
-//                const int & h,
-//                const int & window_size,
-//                const double & tol,
-//                const int & niter_max,
-//                const int & method,
-//                const arma::uword & ncores) {
-//   // This function computes a ODPC over a rolling window.
-//   // INPUT
-//   // data_field: Field with the data to be used for training (could be residuals from previous fits),
-//   // k: value of k to use (k1==k2)
-//   // h: steps ahead
-//   // window_size: the size of the window
-//   // tol: relative precision, stopping criterion
-//   // niter_max: maximum number of iterations
-//   // method: 1 =  ALS, 2 = CD in a, LS in B
-//   // OUTPUT
-//   // a list of the same length as windows_size, each entry being an ODPC
-//   arma::vec nothing = zeros(2);
-//   List output(window_size);
-//   # pragma omp parallel for num_threads(ncores)
-//   for (int ind=0; ind < window_size; ind++){
-//     List ret(1);
-//     ret[0] = odpc_priv(data_field(ind, 0), k, k, nothing, false, tol, niter_max, method);
-//     output[ind] = ret;
-//   }
-//   # pragma omp barrier
-//   return(output);
-// }
-// 
-// // [[Rcpp::export]]
-// List grid_odpc(const arma::field<arma::mat> data_field,
-//                const arma::vec & k_list,
-//                const arma::uword & h,
-//                const arma::uword & window_size,
-//                const double & tol,
-//                const int & niter_max,
-//                const int & method,
-//                const arma::uword & ncores) {
-//   // This function computes a ODPC for each k in k_list, over a rolling window of size window_size.
-//   // INPUT
-//   // data_field: field with the data to be used for training (could be residuals from previous fits),
-//   // one entry per window size
-//   // k_list: list of ks to use (k1==k2)
-//   // h: number of steps ahead, only used for determining the training set
-//   // window_size: the size of the rolling window
-//   // tol: relative precision, stopping criterion
-//   // niter_max: maximum number of iterations
-//   // method: 1 =  ALS, 2 = CD in a, LS in B
-//   // OUTPUT
-//   // a list of the same length as k_list, each entry being a list of ODPCs computed over the rolling window
-//   // for the corresponding value of k
-//   
-//   arma::uword k_list_len = k_list.n_elem;
-//   List output(k_list_len);
-// 
-//   for (arma::uword ind =0; ind < k_list_len; ind++){
-//     output[ind] = roll_odpc(data_field, k_list[ind], h, window_size, tol, niter_max, method, ncores);
-//   }
-//   return(output);
-// }
+// [[Rcpp::export]]
+arma::field<arma::mat> wrap_odpc_priv(const arma::mat & Z,
+                                       const int & k1,
+                                       const int & k2,
+                                       const arma::vec & f_ini,
+                                       const bool & passf_ini,
+                                       const double & tol,
+                                       const int & niter_max,
+                                       const int & method) {
+  // This function computes a single ODPC with a given number of lags.
+  // INPUT
+  // Z: data matrix each column is a different time series
+  // k1: number of lags used to define f
+  // k2: number of lags used to reconstruct
+  // f_ini: initial estimate of f
+  // passf_ini: logical: is f_ini being passed?
+  // tol: relative precision, stopping criterion
+  // niter_max: maximum number of iterations
+  // method: 1 =  ALS, 2 = CD in a, LS in B
+  // OUTPUT
+  // k1: number of lags used to define f
+  // k2: number of lags used to reconstruct
+  // a: vector to construct the principal component
+  // alpha: vector of intercepts corresponding to the principal component
+  // B: matrix of loadings corresponding to the principal component
+  // mse:  mean squared error
+  // conv: logical. Did the iterations converge?
+  // res: matrix of residuals
+  // f: matrix F
+  // criter: last value of 1-mse1/mse0
+  int N = Z.n_rows;
+  int m = Z.n_cols;
+  int k_tot = k1 + k2;
+  arma::mat ident = eye(N - k_tot, N - k_tot);
+  arma::vec vecZ2k = zeros((N - k_tot) * m, 1); //will store vectorise(Z2k) - alphas
+  arma::vec one = ones(N - k_tot, 1);
+  arma::vec alpha = zeros(m, 1);
+  arma::mat Z2k = zeros(N - k_tot, m);
+  arma::mat res = zeros(N - k_tot, m);
+  arma::mat C = zeros((N - k_tot) * (k2 + 1), m * (k1 + 1));
+  arma::mat B = zeros(k2 + 1, m);
+  arma::mat D = zeros(k2 + 2, m);
+  arma::mat matF = zeros(N - k_tot, k2 + 2);
+  matF.col(0).fill(1); //matF's first column is filled with 0
+  arma::vec fout = zeros(N - k1, 1);
+  arma::mat Fitted = zeros(N - k_tot, m);
+  arma::sp_mat W = sp_mat(m * (N - k_tot), (N - k_tot) * (k2 + 1));
+  arma::vec a = zeros(m * (k1 + 1), 1);
+  double mse = 0;
+  int niter = 0;
+  bool conv = false;
+  double criter = tol + 1;
+  Z2k = getMatrixZj(Z, k_tot, k_tot);
+  C = getMatrixC_forecast(Z, k_tot, k1, k2);
+  
+  // if using ALS method
+  if (method == 1){
+    arma::mat WC = zeros(m * (N - k_tot), m * (k2 + 1));
+    // if initial f was passed, get initial a associated with it
+    // else use ordinary PC
+    if (passf_ini) {
+      getVecAini(Z, f_ini, k1, a);
+      getMatrixF(Z, k1, k2, k_tot, a, matF);
+      getMatrixD(Z2k, matF, D);
+      B = D.rows(1, k2 + 1);
+      alpha = D.row(0).t();
+      vecZ2k = vectorise(Z2k) - kron(alpha, ident) * one;
+    } else {
+      matF = getFini_forecast(Z, k1, k2, k_tot);
+      getMatrixD(Z2k, matF, D);
+      B = D.rows(1, k2 + 1);
+      alpha = D.row(0).t();
+      vecZ2k = vectorise(Z2k) - kron(alpha, ident) * one;
+      getVecA(W, WC, B, ident, C, vecZ2k, a);
+      getMatrixF(Z, k1, k2, k_tot, a, matF);
+      getMatrixD(Z2k, matF, D);
+      B = D.rows(1, k2 + 1);
+      alpha = D.row(0).t();
+      vecZ2k = vectorise(Z2k) - kron(alpha, ident) * one;
+    }
+    Fitted = matF * D;
+    mse = getMSE(Z2k, Fitted);
+    double mse_ini = mse;
+    while (niter < niter_max and criter > tol){
+      niter += 1;
+      getVecA(W, WC, B, ident, C, vecZ2k, a);
+      getMatrixF(Z, k1, k2, k_tot, a, matF);
+      getMatrixD(Z2k, matF, D);
+      B = D.rows(1, k2 + 1);
+      alpha = D.row(0).t();
+      vecZ2k = vectorise(Z2k) - kron(alpha, ident) * one;
+      Fitted = matF * D;
+      mse = getMSE(Z2k, Fitted);
+      criter = 1 - mse / mse_ini;
+      mse_ini = mse;
+      // if (niter % 2 == 0){
+      //   Rcpp::checkUserInterrupt();
+      // }
+    }
+    // if using mix method
+  } else if (method == 2){
+    arma::vec out_WCres = zeros((N - k_tot) * m);
+    if (passf_ini) {
+      getVecAini(Z, f_ini, k1, a);
+      getMatrixF(Z, k1, k2, k_tot, a, matF);
+    } else {
+      matF = getFini_forecast(Z, k1, k2, k_tot);
+    }
+    mse = 1e10;
+    double mse_ini = mse;
+    while (niter < niter_max and criter > tol){
+      niter += 1;
+      getVecAMatD(Z2k, matF, ident, C, one, out_WCres, a, alpha, B, D, vecZ2k, W);
+      getMatrixF(Z, k1, k2, k_tot, a, matF);
+      Fitted = matF * D;
+      mse = getMSE(Z2k, Fitted);
+      criter = 1 - mse / mse_ini;
+      mse_ini = mse;
+      // if (niter % 2 == 0){
+      //   Rcpp::checkUserInterrupt();
+      // }
+    }
+  }
+  
+  // check convergence
+  if (niter < niter_max) {
+    conv = true;
+  }
+  
+  if (k2 > 0) {
+    fout(span(0, k2 - 1)) = matF(span(0, k2 - 1), k2 + 1);
+    fout(span(k2, N - k1 - 1)) = matF.col(1);
+  } else {
+    fout = matF.col(1);
+  }
+  
+  res = Z2k - Fitted;
+  arma::vec k1_vec = zeros(1);
+  arma::vec k2_vec = zeros(1);
+  arma::vec mse_vec = zeros(1);
+  k1_vec(0) = k1;
+  k2_vec(0) = k2;
+  mse_vec(0) = mse;
+  arma::field<arma::mat> pre_ret(6, 1);
+  pre_ret(0, 0) = alpha;
+  pre_ret(1, 0) = B;
+  pre_ret(2, 0) = k2_vec;
+  pre_ret(3, 0) = mse_vec;
+  pre_ret(4, 0) = fout;
+  pre_ret(5, 0) = res;
+  return(pre_ret);
+}
+
+// [[Rcpp::export]]
+arma::field<arma::field<arma::mat> > roll_odpc(const arma::field<arma::mat> & data_field,
+                                               const arma::uword & k,
+                                               const int & window_size,
+                                               const double & tol,
+                                               const int & niter_max,
+                                               const int & method,
+                                               const arma::uword & ncores) {
+  // This function computes a ODPC over a rolling window.
+  // INPUT
+  // data_field: Field with the data to be used for training (could be residuals from previous fits),
+  // k: value of k to use (k1==k2)
+  // window_size: the size of the window
+  // tol: relative precision, stopping criterion
+  // niter_max: maximum number of iterations
+  // method: 1 =  ALS, 2 = CD in a, LS in B
+  // OUTPUT
+  // a list of the same length as windows_size, each entry being an ODPC
+  arma::vec nothing = zeros(2);
+  arma::field<arma::field<arma::mat> >  output(window_size, 1);
+  # pragma omp parallel for num_threads(ncores)
+  for (int ind=0; ind < window_size; ind++){
+    output(ind, 0) = wrap_odpc_priv(data_field(ind, 0), k, k, nothing, false, tol, niter_max, method);
+  }
+  # pragma omp barrier
+  return(output);
+}
